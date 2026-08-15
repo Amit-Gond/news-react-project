0eea201ec478d88fa39297c90f4079386a89ccfe
Amit gond <godvinod002@gmail.com>
2026-08-15 14:16:35 +0530

    env file added

--- src/Components/News.js
+++ src/Components/News.js
@@ -76,7 +76,7 @@ export default class News extends Component {
 
   async updateNews() {
     this.props.setProgress(10);
-    const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=0e92d54a98324fd592561f708246dd55&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+    const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apiKey}&page=${this.state.page}&pageSize=${this.props.pageSize}`;
     this.setState({ loading: true });
     this.props.setProgress(30);
     let data = await fetch(url);
